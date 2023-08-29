7ccb266592b6b73111514f53befb230d4315dc07
prashanth <prashanthrjkp@gmail.com>
2023-08-29 16:28:31 +0530

    commited webapps-2

---
+++ src/main/java/com/example/demo/WeBapps2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeBapps2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeBapps2Application.class, args);
+	}
+
+}
